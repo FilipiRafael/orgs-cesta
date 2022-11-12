5c3263fd7e765c7c580688953f623acc9b2604ed
Filipi Rafael <filipirafael.123@gmail.com>
2022-11-12 20:47:23 -0300

    Add new updates

--- src/screens/Cesta.js
+++ src/screens/Cesta.js
@@ -1,5 +1,6 @@
-import { Image, StyleSheet, Dimensions, Text, SafeAreaContent } from "react-native";
+import { Image, StyleSheet, Dimensions, Text, View } from "react-native";
 import topo from "../../assets/topo.png";
+import logo from "../../assets/logo.png";
 
 const width = Dimensions.get('screen').width;
 
@@ -8,6 +9,18 @@ export function Cesta() {
         <>
             <Image source={topo} style={styles.topo} />
             <Text style={styles.title}>Detalhe da cesta</Text>
+
+            <View style={styles.cesta}>
+                <Text style={styles.nome}>Cesta de verduras</Text>
+                <View style={styles.fazenda}>
+                    <Image source={logo} style={styles.imagemFazenda} />
+                    <Text style={styles.nomeFazenda}>Jenny Jack Farm</Text>
+                </View>
+                <Text style={styles.descricao}>
+                    Uma cesta com produtos selecionados cuidadosamente da fazenda direto para sua cozinha.
+                </Text>
+                <Text style={styles.preco}>R$ 40,00</Text>
+            </View>
         </>
     )
 }
@@ -16,6 +29,7 @@ const styles = StyleSheet.create({
     topo: {
         width: '100%',
         height: 578 / 768 * width,
+        fontFamily: 'Montserrat'
     },
     title: {
         width: '100%',
@@ -26,6 +40,41 @@ const styles = StyleSheet.create({
         color: 'white',
         fontWeight: 'bold',
         padding: 16,
-        top: '20%'
+        top: '15%'
+    },
+    cesta: {
+        paddingVertical: 8,
+        paddingHorizontal: 16
+    },
+    nome: {
+        color: '#464646',
+        fontSize: 26,
+        lineHeight: 42,
+        fontWeight: 'bold'
+    },
+    fazenda: {
+        flexDirection: 'row',
+        paddingVertical: 12
+    },
+    imagemFazenda: {
+        width: 32,
+        height: 32
+    },    
+    nomeFazenda: {
+        fontSize: 16,
+        lineHeight: 26,
+        marginLeft: 12
+    },
+    descricao: {
+        color: '#A3A3A3',
+        fontSize: 16,
+        lineHeight: 26
+    },
+    preco: {
+        color: '#2A9F85',
+        fontWeight: 'bold',
+        fontSize: 26,
+        lineHeight: 42,
+        marginTop: 8
     }
 });
