c98ee2fba7eaf4674b31eb0fedb719b643be90f9
Filipi Rafael <filipirafael.123@gmail.com>
2022-11-12 20:57:12 -0300

    Add new updates to app

--- App.js
+++ App.js
@@ -1,7 +1,22 @@
-import { StatusBar, SafeAreaView } from 'react-native';
+import { StatusBar, SafeAreaView, View } from 'react-native';
+import { 
+  useFonts,
+  Montserrat_400Regular,
+  Montserrat_700Bold
+} from '@expo-google-fonts/montserrat';
+
 import { Cesta } from './src/screens/Cesta';
 
 export default function App() {
+  const [fontLoaded] = useFonts({
+    'MontserratRegular': Montserrat_400Regular,
+    'MontserratBold': Montserrat_700Bold
+  });
+
+  if (!fontLoaded) {
+    return <View />
+  }
+
   return (
     <SafeAreaView>
       <StatusBar />
